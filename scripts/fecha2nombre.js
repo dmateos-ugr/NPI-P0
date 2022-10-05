b3f26c87be072268602cfc71a9b91016febb5db8
Salvador Domingues <salvadordomingues@gmail.com>
2022-10-05 17:19:14 +0200

    fecha2nombre

---
+++ scripts/fecha2nombre.js
@@ -0,0 +1,58 @@
+function fechaAnombre(fecha){
+    var nombre = "";
+    if(fecha=="Jueves, 20 de Enero")
+        nombre="Algebra Lineal y Estructuras Matematicas";
+    if(fecha=="Lunes, 24 de Enero")
+        nombre="Calculo";
+    if(fecha=="Miercoles, 26 de Enero")
+        nombre="Fundamentos de Programacion";
+    if(fecha=="Martes, 18 de Enero")
+        nombre="Fundamentos del Software";
+    if(fecha=="Viernes, 14 de Enero")
+        nombre="Fundamentos Fisicos y Tecnologicos";
+    if(fecha=="Miercoles, 12 de Enero")
+        nombre="Estructura de computadores";
+    if(fecha=="Viernes, 21 de Enero")
+        nombre="Estructura de Datos";
+    if(fecha=="Lunes, 17 de Enero")
+        nombre="Programacion y Diseno Orientado a Objetos";
+    if(fecha=="Miercoles, 19 de Enero")
+        nombre="Sistemas Concurrentes y Distribuidos";
+    if(fecha=="Martes, 25 de Enero")
+        nombre="Sistemas Operativos";
+    if(fecha=="Miercoles, 26 de Enero")
+        nombre="Diseno y Desarrollo de Sistemas de Informacion";
+    if(fecha=="Lunes, 24 de Enero")
+        nombre="Fundamentos de Redes";
+    if(fecha=="Viernes, 14 de Enero")
+        nombre="Informatica Grafica";
+    if(fecha=="Jueves, 20 de Enero")
+        nombre="Ingenieria de Servidores";
+    if(fecha=="Lunes, 17 de Enero")
+        nombre="Modelos de Computacion";
+    if(fecha=="Lunes, 24 de Enero")
+        nombre="Nuevos Paradigmas de Interaccion";
+    if(fecha=="Miercoles, 19 de Enero")
+        nombre="Procesadores de Lenguajes";
+    if(fecha=="Miercoles, 12 de Enero")
+        nombre="Vision por Computador";
+    if(fecha=="Lunes, 17 de Enero")
+        nombre="Programacion Tecnica y Cientifica";
+    if(fecha=="Viernes, 21 de Enero")
+        nombre="Teoria de la Informacion y la Codificacion";
+    if(fecha=="Viernes, 14 de Enero")
+        nombre="Simulacion de Sistemas";
+    if(fecha=="Lunes, 24 de Enero")
+        nombre="Centros de Procesamiento de Datos";
+    if(fecha=="Miercoles, 19 de Enero")
+        nombre="Sistemas Empotrados";
+    if(fecha=="Miercoles, 12 de Enero")
+        nombre="Tecnologias de Red";
+    if(fecha=="Lunes, 17 de Enero")
+        nombre="Informatica Industrial";
+    if(fecha=="Viernes, 21 de Enero")
+        nombre="Tecnologias Emergentes";
+    if(fecha=="Lunes, 24 de Enero")
+        nombre="Desarrollo Basado en Agentes";
+    return nombre;
+}
