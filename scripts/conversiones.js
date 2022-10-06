5a392337f9c64103c56b1482961f0eb34fb17239
Salvador Domingues <salvadordomingues@gmail.com>
2022-10-06 11:44:10 +0200

    english and spanish updates

--- scripts/conversiones.js
+++ scripts/conversiones.js
@@ -145,3 +145,156 @@ function fechaAnombre(fecha){
         nombre="Fundamentos de Programacion y Diseno y Desarrollo de Sistemas de Informacion";
     return nombre;
 }
+
+
+////////////////////////////////////////////////////////////////////////////
+
+
+
+function acronymToName(sigla){
+    var nombre = "";
+    sigla = sigla.replace(/ /g, ''); // eliminar espacios
+    if(sigla=="LAMS")
+        nombre="Linear Algebra and Mathematical Structures";
+    else if(sigla=="CA")
+        nombre="Calculus";
+    else if(sigla=="FP")
+        nombre="Fundamentals of Programming";
+    else if(sigla=="FS")
+        nombre="Fundamentals of Software";
+    else if(sigla=="FPT")
+        nombre="Fundamentals of Physics and Technology";
+    else if(sigla=="CS")
+        nombre="Computer Structures";
+    else if(sigla=="DS")
+        nombre="Data Structures";
+    else if(sigla=="OOPD")
+        nombre="Object-Oriented Programming and Design";
+    else if(sigla=="CDS")
+        nombre="Concurrent and Distributed Systems";
+    else if(sigla=="OS")
+        nombre="Operating Systems";
+    else if(sigla=="ISDD")
+        nombre="Information Systems Design and Development";
+    else if(sigla=="BPN")
+        nombre="Basic Principles of Networks";
+    else if(sigla=="GC")
+        nombre="Graphical Computing";
+    else if(sigla=="SE")
+        nombre="Server Engineering";
+    else if(sigla=="MC")
+        nombre="Models of Computation";
+    else if(sigla=="NIP")
+        nombre="New Interaction Paradigms";
+    else if(sigla=="LP")
+        nombre="Language Processors";
+    else if(sigla=="CV")
+        nombre="Computer Vision";
+    else if(sigla=="TSP")
+        nombre="Technical and Scientific Programming";
+    else if(sigla=="TIC")
+        nombre="Theory of Information and Coding";
+    else if(sigla=="SS")
+        nombre="System Simulation";
+    else if(sigla=="DPC")
+        nombre="Data Processing Centres";
+    else if(sigla=="ES")
+        nombre="Embedded Systems";
+    else if(sigla=="NT")
+        nombre="Network Technologies";
+    else if(sigla=="IC")
+        nombre="Industrial Computing";
+    else if(sigla=="ET")
+        nombre="Emerging Technologies";
+    else if(sigla=="ABD")
+        nombre="Agent-Based Development";
+    else
+        nombre="unknown";
+    return nombre;
+}
+
+function nameToDate(nombre){
+    var fecha = "";
+    if(nombre=="Linear Algebra and Mathematical Structures")
+        fecha="Thursday, 20th of January";
+    else if(nombre=="Calculus")
+        fecha="Monday, 24th of January";
+    else if(nombre=="Fundamentals of Programming")
+        fecha="Wednesday, 26th of January";
+    else if(nombre=="Fundamentals of Software")
+        fecha="Tuesday, 18th of January";
+    else if(nombre=="Fundamentals of Physics and Technology")
+        fecha="Friday, 14th of January";
+    else if(nombre=="Computer Structures")
+        fecha="Wednesday, 12nd of January";
+    else if(nombre=="Data Structures")
+        fecha="Friday, 21st of January";
+    else if(nombre=="Object-Oriented Programming and Design")
+        fecha="Monday, 17th of January";
+    else if(nombre=="Concurrent and Distributed Systems")
+        fecha="Wednesday, 19th of January";
+    else if(nombre=="Operating Systems")
+        fecha="Tuesday, 25th of January";
+    else if(nombre=="Information Systems Design and Development")
+        fecha="Wednesday, 26th of January";
+    else if(nombre=="Basic Principles of Networks")
+        fecha="Monday, 24th of January";
+    else if(nombre=="Graphical Computing")
+        fecha="Friday, 14th of January";
+    else if(nombre=="Server Engineering")
+        fecha="Thursday, 20th of January";
+    else if(nombre=="Models of Computation")
+        fecha="Monday, 17th of January";
+    else if(nombre=="New Interaction Paradigms")
+        fecha="Monday, 24th of January";
+    else if(nombre=="Language Processors")
+        fecha="Wednesday, 19th of January";
+    else if(nombre=="Computer Vision")
+        fecha="Wednesday, 12nd of January";
+    else if(nombre=="Technical and Scientific Programming")
+        fecha="Monday, 17th of January";
+    else if(nombre=="Theory of Information and Coding")
+        fecha="Friday, 21st of January";
+    else if(nombre=="System Simulation")
+        fecha="Friday, 14th of January";
+    else if(nombre=="Data Processing Centres")
+        fecha="Monday, 24th of January";
+    else if(nombre=="Embedded Systems")
+        fecha="Wednesday, 19th of January";
+    else if(nombre=="Network Technologies")
+        fecha="Wednesday, 12nd of January";
+    else if(nombre=="Industrial Computing")
+        fecha="Monday, 17th of January";
+    else if(nombre=="Emerging Technologies")
+        fecha="Friday, 21st of January";
+    else if(nombre=="Agent-Based Development")
+        fecha="Monday, 24th of January";
+    else
+        fecha="Unknown course: " + nombre;
+    return fecha;
+}
+
+function dateToName(fecha){
+    var nombre = "";
+    if(fecha=="12nd of January")
+        nombre="Computer Structures, Computer Vision and Network Technologies";
+    else if(fecha=="14th of January")
+        nombre="Fundamentals of Physics and Technology, Graphical Computing and System Simulation";
+    else if(fecha=="17th of January")
+        nombre="Object-Oriented Programming and Design, Models of Computation, Technical and Scientific Programming and Industrial Computing";
+    else if(fecha=="18th of January")
+        nombre="Fundamentals of Software";
+    else if(fecha=="19th of January")
+        nombre="Concurrent and Distributed Systems, Language Processors and Embedded Systems";
+    else if(fecha=="20th of January")
+        nombre="Linear Algebra and Mathematical Structures and Server Engineering";
+    else if(fecha=="21st of January")
+        nombre="Data Structures, Theory of Information and Coding and Emerging Technologies";
+    else if(fecha=="24th of January")
+        nombre="Calculus, Basic Principles of Networks, New Interaction Paradigms, Data Processing Centres and Agent-Based Development";
+    else if(fecha=="25th of January")
+        nombre="Operating Systems";
+    else if(fecha=="26th of January")
+        nombre="Fundamentals of Programming and Information Systems Design and Development";
+    return nombre;
+}
